dfcb47fdc289f88321d2e8d40abab7f6a3038004
JulianeFarmer <juliane-katharina.farmer@student.howest.be>
2025-05-31 09:56:50 +0200

    updated code & trained model 31/05

--- new_bird.py
+++ new_bird.py
@@ -1,21 +1,23 @@
+import flappy_bird_gymnasium     
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 
-env = gym.make("FlappyBird-v0")     
-eval_env = gym.make("FlappyBird-v0")
 
-eval_callback = EvalCallback(
+train_env      = gym.make("FlappyBird-v0")                # no render
+eval_env = gym.make("FlappyBird-v0", render_mode="human")  # you’ll see it play
+
+eval_cb = EvalCallback(
     eval_env,
     best_model_save_path="./logs/best_model/",
     log_path="./logs/results/",
     eval_freq=10_000,
     n_eval_episodes=5,
-    deterministic=True,)
+    deterministic=True,
+)
 
 model = PPO(
-    "MlpPolicy",
-    env,
+    "MlpPolicy", train_env,
     learning_rate=3e-4,
     n_steps=2048,
     batch_size=64,
@@ -23,15 +25,17 @@ model = PPO(
     verbose=1,
     tensorboard_log="./logs/tb/",
 )
-model.learn(total_timesteps=1_000_000, callback=eval_callback)
+model.learn(total_timesteps=1_000_000, callback=eval_cb)
 
-model.save("ppo_flappy")
+model.save("/mnt/c/Users/julia/OneDrive/Desktop/flappy_bird/"
+           "flappy_bird_project_farmer/model/ppo_flappy")
 
-obs, _ = env.reset()
+play_env = gym.make("FlappyBird-v0", render_mode = 'human')
+obs, _ = play_env.reset()
 for _ in range(1_000):
     action, _ = model.predict(obs, deterministic=True)
-    obs, reward, done, _, info = env.step(action)
-    env.render()
+    obs, _, done, _, _ = play_env.step(action)
+    play_env.render()
     if done:
-        obs, _ = env.reset()
-env.close()
+        obs, _ = play_env.reset()
+play_env.close()
--- play_bird.py
+++ play_bird.py
@@ -1,26 +1,26 @@
 import argparse
+import flappy_bird_gymnasium 
 import gymnasium as gym
 from stable_baselines3 import PPO
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument(
-        "--model-path",
+        "--model-path",  
         type=str,
         required=True,
         help="Path to the trained model (.zip) to load"
     )
     args = p.parse_args()
 
-    env = gym.make("FlappyBird-v0")
+    env = gym.make("FlappyBird-v0", render_mode="human")
     obs, _ = env.reset()
 
     model = PPO.load(args.model_path, env=env)
 
-    done = False
     while True:
         action, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, _, info = env.step(action)
+        obs, reward, done, _, _ = env.step(action)
         env.render()
         if done:
             obs, _ = env.reset()
